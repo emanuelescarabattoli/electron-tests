da98b16dea8885083af9023b655aa739b4b82f78
Emanuele Scarabattoli <emanuele.scarabattoli@evonove.it>
2019-10-31 14:33:16 +0100

    Window frame

--- src/containers/Application/components/Container/index.js
+++ src/containers/Application/components/Container/index.js
@@ -6,7 +6,14 @@ import style from "./style.scss";
 const Container = ({ children }) => (
   <>
     <div className={style.drag}>
-      Title
+      <div>
+        <span className={style.title}>Title</span>
+      </div>
+      <div className={style.actions}>
+        <button className={style.action}><i className="material-icons">minimize</i></button>
+        <button className={style.action}><i className="material-icons">crop_square</i></button>
+        <button className={style.action}><i className="material-icons">close</i></button>
+      </div>
     </div >
     <div>
       {children}
