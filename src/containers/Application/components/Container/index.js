b3ba83eca70dbf234df2d76eb4d4de5ed3992d1d
Emanuele Scarabattoli <emanuele.scarabattoli@evonove.it>
2019-10-31 17:40:33 +0100

    Layout

--- src/containers/Application/components/Container/index.js
+++ src/containers/Application/components/Container/index.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 import style from "./style.scss";
 
 const Container = ({ children }) => (
-  <>
+  <div className={style.window}>
     <div className={style.drag}>
       <div>
         <span className={style.title}>Title</span>
@@ -15,10 +15,37 @@ const Container = ({ children }) => (
         <button className={style.action}><i className="material-icons">close</i></button>
       </div>
     </div >
-    <div>
-      {children}
+    <div className={style.wrapper}>
+      <div className={style.sidebarWrapper}>
+        <div className={style.sidebar}>
+          <div>
+            <ul className={style.list}>
+              <li>
+                <i className="material-icons">explore</i>
+              </li>
+              <li>
+                <i className="material-icons">favorite</i>
+              </li>
+              <li>
+                <i className="material-icons">drafts</i>
+              </li>
+            </ul>
+            <ul className={style.list}>
+              <li>
+                <i className="material-icons">account_circle</i>
+              </li>
+              <li>
+                <i className="material-icons">settings</i>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className={style.content}>
+        {children}
+      </div>
     </div>
-  </>
+  </div>
 );
 
 Container.propTypes = {
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,9 +1,54 @@
 import React from "react";
 
+import style from "./style.scss";
+
 const Home = () => {
   return (
     <div className="drag">
-      <div>foo</div>
+      <span className={style.tagTitle}>Explore</span>
+      <span className={style.title}>Your favorite items</span>
+      <div className={style.layout}>
+        <div>
+          <div className={style.card}>
+            <div className={style.cardHeader}>
+              Hello
+            </div>
+            <div className={style.cardBody}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </div>
+            <div className={style.cardFooter}>
+              <button className={style.button}>find out more</button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className={style.card}>
+            <div className={style.cardHeader}>
+              Hello
+            </div>
+            <div className={style.cardBody}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </div>
+            <div className={style.cardFooter}>
+              <button className={style.button}>foo</button>
+              <button className={style.button}>bar</button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className={style.card}>
+            <div className={style.cardHeader}>
+              Hello
+            </div>
+            <div className={style.cardBody}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </div>
+            <div className={style.cardFooter}>
+              <button className={style.button}>find out more</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
